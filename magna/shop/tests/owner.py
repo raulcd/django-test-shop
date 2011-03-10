eca48ba5e7f3308834ae75cbdfde6ea49f01012a
Raúl Cumplido <raulcumplido@gmail.com>
2011-03-10 17:50:16 +0100

    Added test for owner email but it works when it has to fail. Modify it

--- magna/shop/tests/owner.py
+++ magna/shop/tests/owner.py
@@ -7,9 +7,17 @@ class OwnerTestCase(unittest.TestCase):
         self.owneraccount = OwnerAccount()
         self.owneraccount.first_name = "Raúl"
         self.owneraccount.last_name = "Cumplido Domínguez"
+        self.owneraccount.email = "raulcumplido@gmail.com"
         self.owneraccount.save()
 
     def test_01_unicode_returns_proper_stuff(self):
         self.create_fixtures()
         ret = self.owneraccount.__unicode__()
         self.assertEqual(ret, self.owneraccount.first_name)
+
+    def test_02_create_owner_account_with_wrong_email(self):
+        self.owneraccount_2 = OwnerAccount()
+        self.owneraccount_2.first_name = "Raúl"
+        self.owneraccount_2.last_name = "Cumplido Domínguez"
+        self.owneraccount_2.email = "raulcd"
+        self.owneraccount_2.save()
