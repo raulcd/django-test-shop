5b11dae639cd8f5c21d0b8d3a5dabe91d6a1bcc4
raulcd <raulcumplido@gmail.com>
2011-03-11 20:32:28 +0100

    Added init file for coverage. Also added script to generate tests and docs easyly

---
+++ runtests.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+args=("$@")
+num_args=${#args[@]}
+index=0
+
+suite='shop'
+
+coverage=false
+documentation=false
+
+while [ "$index" -lt "$num_args" ]
+do
+	case "${args[$index]}" in
+		"--with-docs")
+			documentation=true
+			;;
+		"--with-coverage")
+			coverage=true
+			;;
+		*)
+			suite="shop.${args[$index]}"
+	esac
+let "index = $index + 1"
+done
+
+if [ $coverage == true ]; then
+	pushd .
+	cd magna 
+	coverage run manage.py test $suite
+	coverage html
+	x-www-browser htmlcov/index.html
+	popd
+
+else
+
+	# the default case...
+	pushd .
+	cd magna
+	python manage.py test $suite
+	popd
+
+fi
+
+if [ $documentation == true ]; then
+	pushd .
+	cd docs/
+	make html
+	x-www-browser _build/html/index.html
+	popd
+fi
