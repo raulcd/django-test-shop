98e35cf79c0fe8bb4611003423b6510cf9cccf9b
raulcd <raulcumplido@gmail.com>
2011-03-12 13:17:45 +0100

    When executing create_shop insert data into the database

--- magna/shop/views.py
+++ magna/shop/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from shop.forms import CreateOwnerForm
+from shop.models.owner_account_model import OwnerAccount
+from shop.models.shop_model import Shop
 from django.core.urlresolvers import reverse
 
 def create_shop(request):
-    # TODO: Error control
     # View if the method is POST and the form is valid has data attached
     if request.method == 'POST':
         form = CreateOwnerForm(request.POST)
         if form.is_valid():
+            owner = OwnerAccount()
+            owner.first_name = form.cleaned_data['first_name']
+            owner.last_name = form.cleaned_data['last_name']
+            owner.email = form.cleaned_data['email']
+            owner.save()
+            shop = Shop()
+            shop.owner = owner
+            shop.name = form.cleaned_data['store_name']
+            shop.save()
             # We need to do something with the data
             return HttpResponseRedirect(reverse('shop.views.thanks'))
     else:
